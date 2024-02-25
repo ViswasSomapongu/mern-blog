03334735031c187417d0c44a5f2c94e0fc611028
Viswas Somapongo <viswass810@gmail.com>
2024-02-25 20:25:40 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+mongoose.connect(
+    process.env.MONGO
+)
+.then(() => {
+    console.log("MongoDB is connected...")
+})
+.catch((err) => {
+    console.log(err);
+})
+
 const app = express();
 const port = 3000;
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on port number ${port}`)
 })
