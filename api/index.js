401235c6f3ac05c5e79e6275aa9f7c1385f9c6f3
Viswas Somapongo <viswass810@gmail.com>
2024-02-26 17:19:21 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ mongoose.connect(
 })
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 
@@ -24,3 +26,4 @@ app.listen(port, () => {
 })
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
