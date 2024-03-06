87c9ae5d63075a014223297829e0b9000908ed4b
Viswas Somapongo <viswass810@gmail.com>
2024-03-06 22:54:39 +0530

    complete create a post page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,14 +5,15 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from './../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updatefailure,deleteUserStart,deleteUserSuccess,deleteUserfailure,signOutSuccess } from '../redux/user/userSlice';
-import {HiOutlineExclamationCircle} from 'react-icons/hi'
+import { updateStart, updateSuccess, updatefailure, deleteUserStart, deleteUserSuccess, deleteUserfailure, signOutSuccess } from '../redux/user/userSlice';
+import { HiOutlineExclamationCircle } from 'react-icons/hi'
 import { signout } from '../../../api/controllers/user.controller';
+import { Link } from 'react-router-dom';
 
 
 const DashProfile = () => {
 
-  const { currentUser,error } = useSelector((state) => state.user)
+  const { currentUser, error, loading} = useSelector((state) => state.user)
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -126,36 +127,36 @@ const DashProfile = () => {
     }
   }
 
-  const handleDeleteUser = async ()=>{
-        setShowModal(false)
-        try {
-          dispatch(deleteUserStart())
-          const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-            method: 'DELETE',
-          });
-          const data = await res.json();
-          if (!res.ok) {
-            dispatch(deleteUserfailure(data.message));
-          
-          } else {
-            dispatch(deleteUserSuccess(data));
-          }
-        } catch (error) {
-          dispatch(deleteUserfailure(error.message))
+  const handleDeleteUser = async () => {
+    setShowModal(false)
+    try {
+      dispatch(deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(deleteUserfailure(data.message));
 
-        }
+      } else {
+        dispatch(deleteUserSuccess(data));
+      }
+    } catch (error) {
+      dispatch(deleteUserfailure(error.message))
+
+    }
   }
-  const handleSignout = async () =>{
+  const handleSignout = async () => {
     try {
       const res = await fetch(`/api/user/signout`, {
         method: 'POST',
       });
       const data = await res.json();
-          if (!res.ok) {
-            console.log(data.message)
-          } else {
-          dispatch(signOutSuccess())
-          }
+      if (!res.ok) {
+        console.log(data.message)
+      } else {
+        dispatch(signOutSuccess())
+      }
     } catch (error) {
       console.log(error.message)
     }
@@ -194,12 +195,22 @@ const DashProfile = () => {
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange} />
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange} />
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange} />
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-          Update
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+          {loading ? 'Loading...':'Update'}
         </Button>
+        {
+       
+          currentUser.isAdmin && (
+            <Link to='/create-post'>
+            <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>
+              Create a post
+            </Button>
+            </Link>
+          )
+        }
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
-        <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
+        <span onClick={() => setShowModal(true)} className='cursor-pointer'>Delete Account</span>
         <span onClick={handleSignout} className='cursor-pointer'>Sign Out</span>
       </div>
       {
@@ -223,20 +234,20 @@ const DashProfile = () => {
           </Alert>
         )
       }
-      <Modal show={showModal} onClose={()=> setShowModal(false)} popup size='md'>
-       <Modal.Header/>
-       <Modal.Body>
-        <div className="text-center">
-          <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/>
-          <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>Are you sure you want to delete your account?</h3>
-           <div className='flex justify-center gap-4'>
-            <Button color='failure' onClick={handleDeleteUser}>
-             Yes, I'm sure
-            </Button>
-            <Button color='gray' onClick={()=>setShowModal(false)}>No, cancel</Button>
-           </div>
-        </div>
-       </Modal.Body>
+      <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>Are you sure you want to delete your account?</h3>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteUser}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>No, cancel</Button>
+            </div>
+          </div>
+        </Modal.Body>
       </Modal>
     </div>
   )
